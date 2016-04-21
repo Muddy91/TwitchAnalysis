fe024485c70ab45247da9d0513afad66528269a6
Freddeb <f.bredmar@gmail.com>
2016-04-21 17:28:45 +0200

    Added support for raw file upload and some smoother file structure

--- S3_handle.py
+++ S3_handle.py
@@ -5,15 +5,25 @@ import threading
 import boto3
 import time
 import sys
+import os
 _lock = threading.Lock()
 _s3 = boto3.resource('s3')
 _bucket = _s3.Bucket('twitch-brede')
 file_list = []
 
-# Function to upload a file from a given filepath
+# Function to upload a file from a given file path
 def upload_file(file_path):
+    # Abort if we try to upload empty file!
+    if os.path.getsize(file_path) <= 0:
+        return
+    date_suffix = '_' + time.strftime("%d%m%y")
     f = open(file_path, 'rb')
+    fr = open(file_path+"_raw", 'rb')
+    splits = file_path.split('/')
+    raw_path = "raw/" + splits[1:]
     _lock.acquire()
-    _bucket.put_object(Key=file_path.split('/')[-1], Body=f)
+    _bucket.put_object(Key=f.name+date_suffix, Body=f)
+    _bucket.put_object(Key=raw_path+date_suffix, Body=fr)
     _lock.release()
+    f.close()
 
