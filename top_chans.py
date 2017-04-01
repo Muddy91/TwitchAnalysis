235bc29e184f5597d1c8e7cbd0e472aed9d777b2
brdmr <f.bredmar@gmail.com>
2017-04-01 23:54:57 +0200

    Retry to load top channels on error

--- top_chans.py
+++ top_chans.py
@@ -1,21 +1,38 @@
 import urllib2
 import json
 import credentials as creds
+from time import sleep
 """
  Module to extract information from top channels
 """
 
 
 class TopChans:
+  max_retries = 10
+  wait_retry_sec = 5
   url = "https://api.twitch.tv/kraken/streams"
   # Return current top channels in JSON format, number of channels is num
   def top_channels(self, num):
-    curr_url = self.url + "?limit=" + str(num) + "&stream_type=live"
-    request = urllib2.Request(curr_url, headers={"Client-ID": creds.T_CLIENTID})
-    req = urllib2.urlopen(request)
-    data = req.read()
-    res = self.data_to_json(data)
-    return res
+    num_retries = 0
+    while num_retries < self.max_retries:
+      try:
+        curr_url = self.url + "?limit=" + str(num) + "&stream_type=live"
+        request = urllib2.Request(curr_url, headers={"Client-ID": creds.T_CLIENTID})
+        req = urllib2.urlopen(request)
+        data = req.read()
+        res = self.data_to_json(data)
+        return res
+      except Exception, e:
+        print e
+        num_retries = num_retries + 1
+        if num_retries < self.max_retries:
+          print "Will retry in %d seconds", wait_retry_sec
+          sleep(wait_retry_sec)
+        else:
+          print "Final retry, won't retry again"
+
+
+
 
   # Convert string data to json
   def data_to_json(self, data):
